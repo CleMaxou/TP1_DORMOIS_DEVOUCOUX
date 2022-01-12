c3fb24b2e2c914e890271d76e56b09f73d863711
webclement <85169363+webclement@users.noreply.github.com>
2022-01-12 13:31:16 +0100

    Add files via upload

---
+++ bibliotheque.h
@@ -0,0 +1,28 @@
+#ifndef BIBLIOTHEQUE_H
+#define BIBLIOTHEQUE_H
+
+#include <vector>
+#include <string>
+#include "Livre.h"
+#include "Lecteur.h"
+#include "Emprunt.h"
+
+class Bibliotheque {
+public:
+	Bibliotheque();
+	void ajouterLivre(Livre& livre);
+	void ajouterUnLecteur(Lecteur& lecteur);
+	void ajouterEmprunt(Emprunt& emprunt);
+	void afficherListeLivre();
+	void afficherListeLecteur();
+	void afficherListeEmprunt();
+	void emprunt(Livre& livre, Lecteur& lecteur, Date date);
+	void rendre(Livre& livre, Lecteur& lecteur, int numEmprunt);
+private:
+	std::vector<Livre> _listeLivres;
+	std::vector<Lecteur> _listeLecteurs;
+	std::vector<Emprunt> _listeEmprunts;
+
+};
+
+#endif // BIBLIOTHEQUE_H
--- main.cpp
+++ main.cpp
@@ -1,132 +1,37 @@
-#include <iostream>
-#include "Livre.h"
-#include "Auteur.h"
-#include "Date.h"
-#include "Lecteur.h"
-#include "Emprunt.h"
-#include "Bibliotheque.h"
-
-int main()
-{
-    // objets
-    Bibliotheque bibli1;
-
-    Date date1(5,13,2020);
-    Date date2(2,25,1995);
-    Date date3(8,31,1959);
-    Date date4(11,25,2022);
-
-    Auteur auteur1("Colbell", "Jonhatan", date3, "7854");
-    Auteur auteur2("Blora", "Natasha", date2, "8541");
-
-    Livre Direction("Direction", "francais", "historique", "254106", auteur1, date1);
-    Livre Nemo("Nemo", "allemand", "roman", "125652", auteur2, date1);
-    Livre LPCR("Le petit chaperon rouge", "anglais", "conte", "454587", auteur2, date1);
-
-    Lecteur marc("123", "BOUELAU", "Marc");
-    Lecteur clement("245", "DORMOIS", "Clement");
-    Lecteur maxou("921", "DEVOUCOUX", "Maxime");
-
-    //Livre *pointDirection = &Direction;
-    // ========== pointeurs ======================
-    Lecteur *pointMarc = &marc;
-    Lecteur *pointClement = &clement;
-    Lecteur *pointMaxou = &maxou;
-
-    // ajout de livres
-    bibli1.ajouterUnLivre(Direction);
-    bibli1.ajouterUnLivre(LPCR);
-    bibli1.ajouterUnLivre(Nemo);
-
-    // ajout de lecteurs
-    bibli1.ajouterUnLecteur(clement);
-    bibli1.ajouterUnLecteur(maxou);
-    bibli1.ajouterUnLecteur(marc);
-
-    bibli1.afficherListeLecteur();
-    bibli1.afficherListeLivre();
-
-    bibli1.emprunt(Nemo, maxou, date4);
-    Nemo.afficheLivre();
-
-    bibli1.emprunt(Direction, clement, date4);
-    Direction.afficheLivre();
-
-    bibli1.afficherListeEmprunt();
-
-    bibli1.rendre(Direction, clement, 2);
-    bibli1.rendre(Nemo, maxou, 1);
-
-    Direction.afficheLivre();
-    Nemo.afficheLivre();
-
-    Direction.afficherEmprunt();
-    Nemo.afficherEmprunt();
+#include<iostream>
+#include"date.h"
+#include"livre.h"
+#include"lecteur.h"
+#include"auteur.h"
+#include"emprunt.h"
+#include"bibliotheque.h"
 
 
+using namespace std;
+int main() {
+	Date date1(5, 13, 2020);
+	Date date2(2, 25, 1995);
+	Date date3(8, 31, 1959);
+	Date date4(11, 25, 2022);
 
-    /*
-    std::cout << " ============== marc ============" << std::endl;
+	Auteur auteur1("7854","Colbell", "Jonhatan", date3);
+	Auteur auteur2("8541","Blora", "Natasha", date2);
+	Auteur auteur3("bebe", "werber", "bernard", date1);
+	Auteur auteur4("bebe", "werber", "bernard", date4);
 
-    Direction.emprunter(*pointMarc);
-    Direction.afficheLivre();
-    Direction.afficherEmprunt();
-    Direction.rendre(*pointMarc);
-    Direction.afficheLivre();
-    std::cout << std::endl << std::endl;
+	Livre livre1("The Hitchhiker's Guide to the Galaxy", auteur1, "Anglais", "Science Fiction", date1, "0593359445");
+	Livre livre2("L'armee des fourmis mangeusent domme ", auteur1, "Français", "Documentaire", date2, "6666666");
+	Livre livre3("domme ", auteur2, "Français", "Documentaire", date2, "6666666");
+	Livre livre4("L'armee ", auteur2, "Français", "Documentaire", date2, "6666666");
+	Livre livre5("Hello Kitty", auteur4, "Français", "Documentaire", date3, "6666666");
+	Lecteur ginhac("dginhac", "Ginhac", "Dominique");
+	Lecteur max("dmax", "Devaucoux", "Max");
+	Lecteur mathis("dmathis", "Demonceaux", "Mathis");
+	Bibliotheque biblio;
+	biblio.emprunt(livre1, max, date1);
+	biblio.rendre(livre2, max, 1);
 
-    std::cout << " ============== clement ============" << std::endl;
-    Direction.emprunter(*pointClement);
-    Direction.afficheLivre();
-    Direction.afficherEmprunt();
-    Direction.rendre(*pointClement);
-    Direction.afficheLivre();
-
-    std::cout << " ============== maxou ============" << std::endl;
-    Direction.emprunter(*pointMaxou);
-    Direction.afficheLivre();
-    Direction.afficherEmprunt();
-
-    std::cout << std::endl << std::endl;
-    pointClement->afficherIsbn();   // Ã  faire
-
-    Emprunt emprunt1(date1, Direction, maxou);
-    emprunt1.afficher();
-
-    std::cout << std::endl << std::endl;
-    std::cout << " ==========================" << std::endl;
-    bibli1.ajouterUnLivre(Direction);
-    bibli1.ajouterUnLivre(LFNPDSDM);
-    bibli1.ajouterUnLivre(LPCR);
-    bibli1.afficherListeLivre();
-
-    std::cout << std::endl << std::endl;
-    std::cout << " ==========================" << std::endl;
-
-    bibli1.ajouterUnLecteur(clement);
-    bibli1.ajouterUnLecteur(marc);
-    bibli1.ajouterUnLecteur(maxou);
-
-
-    bibli1.emprunt(Direction,clement,date1);
-
-
-    bibli1.afficherListeEmprunt();
-
-    */
-
-
-
-
-    /*
-    Emprunt emprunt1("0125", "235");
-    Emprunt *pointEmmprunt1 = &emprunt1;
-    pointEmmprunt1->setId("1258");
-    emprunt1.afficher();
-    */
-
-    delete pointMarc;
-    delete pointClement;
-    delete pointMaxou;
-    return 0;
+	
+	
+	return 0;
 }
